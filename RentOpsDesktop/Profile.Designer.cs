eef507132fcff78c9aca52816b6d6ca6abddeb0b
Maryam Mohsen <89767126+Maryam308@users.noreply.github.com>
2025-03-24 15:36:00 +0300

    edit profile

--- RentOpsDesktop/Profile.Designer.cs
+++ RentOpsDesktop/Profile.Designer.cs
@@ -29,14 +29,6 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            picLogo = new PictureBox();
-            lblProfile = new Label();
-            lblUsername = new Label();
-            lblPhone = new Label();
-            lblEmail = new Label();
-            txtUsername = new TextBox();
-            txtPhone = new TextBox();
-            txtEmail = new TextBox();
             btnEquipmentInformation = new Button();
             btnReturnRecords = new Button();
             btnRentalTransactions = new Button();
@@ -45,6 +37,14 @@
             btnProfile = new Button();
             btnRentalRequests = new Button();
             btnEquipmentDashboard = new Button();
+            picLogo = new PictureBox();
+            lblProfile = new Label();
+            lblUsername = new Label();
+            lblPhone = new Label();
+            lblEmail = new Label();
+            txtUsername = new TextBox();
+            txtPhone = new TextBox();
+            txtEmail = new TextBox();
             btnEditProfile = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
@@ -66,84 +66,6 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(267, 612);
             panel1.TabIndex = 0;
-            // 
-            // picLogo
-            // 
-            picLogo.Image = Properties.Resources.rentOpsLogo;
-            picLogo.Location = new Point(65, 12);
-            picLogo.Name = "picLogo";
-            picLogo.Size = new Size(121, 120);
-            picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
-            picLogo.TabIndex = 4;
-            picLogo.TabStop = false;
-            // 
-            // lblProfile
-            // 
-            lblProfile.AutoSize = true;
-            lblProfile.Font = new Font("Segoe UI", 22.8000011F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0, true);
-            lblProfile.ForeColor = Color.FromArgb(135, 66, 62);
-            lblProfile.Location = new Point(367, 55);
-            lblProfile.Name = "lblProfile";
-            lblProfile.Size = new Size(210, 52);
-            lblProfile.TabIndex = 2;
-            lblProfile.Text = "My Profile";
-            // 
-            // lblUsername
-            // 
-            lblUsername.AutoSize = true;
-            lblUsername.Font = new Font("Segoe UI", 17.8000011F);
-            lblUsername.ForeColor = Color.Black;
-            lblUsername.Location = new Point(367, 225);
-            lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(152, 41);
-            lblUsername.TabIndex = 8;
-            lblUsername.Text = "Username";
-            // 
-            // lblPhone
-            // 
-            lblPhone.AutoSize = true;
-            lblPhone.Font = new Font("Segoe UI", 17.8000011F);
-            lblPhone.ForeColor = Color.Black;
-            lblPhone.Location = new Point(367, 319);
-            lblPhone.Name = "lblPhone";
-            lblPhone.Size = new Size(220, 41);
-            lblPhone.TabIndex = 9;
-            lblPhone.Text = "Phone Number";
-            // 
-            // lblEmail
-            // 
-            lblEmail.AutoSize = true;
-            lblEmail.Font = new Font("Segoe UI", 17.8000011F);
-            lblEmail.ForeColor = Color.Black;
-            lblEmail.Location = new Point(367, 413);
-            lblEmail.Name = "lblEmail";
-            lblEmail.Size = new Size(88, 41);
-            lblEmail.TabIndex = 10;
-            lblEmail.Text = "Email";
-            // 
-            // txtUsername
-            // 
-            txtUsername.Font = new Font("Segoe UI", 14F);
-            txtUsername.Location = new Point(630, 231);
-            txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(368, 39);
-            txtUsername.TabIndex = 11;
-            // 
-            // txtPhone
-            // 
-            txtPhone.Font = new Font("Segoe UI", 14F);
-            txtPhone.Location = new Point(630, 324);
-            txtPhone.Name = "txtPhone";
-            txtPhone.Size = new Size(368, 39);
-            txtPhone.TabIndex = 12;
-            // 
-            // txtEmail
-            // 
-            txtEmail.Font = new Font("Segoe UI", 14F);
-            txtEmail.Location = new Point(630, 413);
-            txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(368, 39);
-            txtEmail.TabIndex = 13;
             // 
             // btnEquipmentInformation
             // 
@@ -267,6 +189,84 @@
             btnEquipmentDashboard.TextAlign = ContentAlignment.MiddleLeft;
             btnEquipmentDashboard.UseVisualStyleBackColor = false;
             // 
+            // picLogo
+            // 
+            picLogo.Image = Properties.Resources.rentOpsLogo;
+            picLogo.Location = new Point(65, 12);
+            picLogo.Name = "picLogo";
+            picLogo.Size = new Size(121, 120);
+            picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            picLogo.TabIndex = 4;
+            picLogo.TabStop = false;
+            // 
+            // lblProfile
+            // 
+            lblProfile.AutoSize = true;
+            lblProfile.Font = new Font("Segoe UI", 22.8000011F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0, true);
+            lblProfile.ForeColor = Color.FromArgb(135, 66, 62);
+            lblProfile.Location = new Point(367, 55);
+            lblProfile.Name = "lblProfile";
+            lblProfile.Size = new Size(210, 52);
+            lblProfile.TabIndex = 2;
+            lblProfile.Text = "My Profile";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 17.8000011F);
+            lblUsername.ForeColor = Color.Black;
+            lblUsername.Location = new Point(367, 225);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(152, 41);
+            lblUsername.TabIndex = 8;
+            lblUsername.Text = "Username";
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Font = new Font("Segoe UI", 17.8000011F);
+            lblPhone.ForeColor = Color.Black;
+            lblPhone.Location = new Point(367, 319);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new Size(220, 41);
+            lblPhone.TabIndex = 9;
+            lblPhone.Text = "Phone Number";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new Font("Segoe UI", 17.8000011F);
+            lblEmail.ForeColor = Color.Black;
+            lblEmail.Location = new Point(367, 413);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(88, 41);
+            lblEmail.TabIndex = 10;
+            lblEmail.Text = "Email";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Font = new Font("Segoe UI", 14F);
+            txtUsername.Location = new Point(630, 231);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(368, 39);
+            txtUsername.TabIndex = 11;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Font = new Font("Segoe UI", 14F);
+            txtPhone.Location = new Point(630, 324);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(368, 39);
+            txtPhone.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Segoe UI", 14F);
+            txtEmail.Location = new Point(630, 413);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(368, 39);
+            txtEmail.TabIndex = 13;
+            // 
             // btnEditProfile
             // 
             btnEditProfile.BackColor = Color.FromArgb(139, 128, 116);
@@ -277,7 +277,7 @@
             btnEditProfile.Name = "btnEditProfile";
             btnEditProfile.Size = new Size(173, 47);
             btnEditProfile.TabIndex = 29;
-            btnEditProfile.Text = "Edit Profile";
+            btnEditProfile.Text = "Edit ProfilE";
             btnEditProfile.UseVisualStyleBackColor = false;
             // 
             // Profile
