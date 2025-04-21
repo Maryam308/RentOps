d4aaa4bf4e6cf654364976c7f5e9f6b1320283c5
MariamAhmed004 <mariamisa5w@gmail.com>
2025-04-21 15:18:52 +0300

    corrected scaffold

---
+++ RentOpsObjects/Model/ExternalCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentOpsObjects.Model;
+
+[Table("ExternalCustomer")]
+public partial class ExternalCustomer
+{
+    [Key]
+    [Column("customerID")]
+    public int CustomerId { get; set; }
+
+    [Column("firstName")]
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [Column("lastName")]
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [Column("email")]
+    [StringLength(50)]
+    public string Email { get; set; } = null!;
+
+    [Column("phoneNumber")]
+    [StringLength(10)]
+    public string PhoneNumber { get; set; } = null!;
+}
