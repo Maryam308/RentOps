bebbbc50e53f08698981586b05535524ee1fe8fe
MariamAhmed004 <mariamisa5w@gmail.com>
2025-04-28 23:30:34 +0300

    rental request filtering and view model

--- RentOpsWebApp/ViewModels/RentalRequestViewModel.cs
+++ RentOpsWebApp/ViewModels/RentalRequestViewModel.cs
@@ -6,5 +6,16 @@ namespace RentOpsWebApp.ViewModels
     {
         public IEnumerable<RentalRequest>? rentalRequests { get; set; }
 
+        public IEnumerable<RentalRequestStatus>? rentalRequestStatuses { get; set; }
+
+        public IEnumerable<Equipment>? equipmentTitle { get; set; }
+
+        public string? SearchRentalRequestId { get; set; }
+
+        public string? SearchRentalRequestStatusId { get; set; }
+
+        public string? SearchEquipmentId { get; set; }
+
+
     }
 }
